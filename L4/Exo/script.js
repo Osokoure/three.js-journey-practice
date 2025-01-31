69add9d40608157421e48d3eea9ee5c3cce1cd72
Théo Hofmann <theohp@outlook.fr>
2025-01-31 15:31:50 +0100

    Mid Lecon 4 - Scale

--- L4/Exo/script.js
+++ L4/Exo/script.js
@@ -12,6 +12,19 @@ const material = new THREE.MeshBasicMaterial({ color: "red" })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+mesh.position.set(0,0,0)
+
+//Scale
+
+
+console.log("Position par rapport au centre:",mesh.position.length())
+
+//Axes helper
+
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+axesHelper.position.set(1,1,1)
+
 // Sizes
 const sizes ={
     width: 800,
@@ -22,12 +35,14 @@ const sizes ={
 
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
 scene.add(camera)
-camera.position.z = 3
+camera.position.z = 4
+console.log("Distance de la caméra:",mesh.position.distanceTo(camera.position))
 
 // Renderer 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)

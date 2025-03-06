017905faeea6fbc99c30b044d147bbbe3648c456
Théo Hofmann <theohp@outlook.fr>
2025-03-06 12:28:45 +0100

    Fin Leçon 13

--- L13/src/script.js
+++ L13/src/script.js
@@ -8,7 +8,7 @@ import GUI from 'lil-gui'
  * Base
  */
 // Debug
-const gui = new GUI()
+//const gui = new GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -20,7 +20,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('textures/matcaps/2.png')
+const matcapTexture = textureLoader.load('textures/matcaps/8.png')
 matcapTexture.colorSpace = THREE.SRGBColorSpace
 
 /**
@@ -38,7 +38,23 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Theo HOFMANN',
+            {
+                font: font,
+                size: 0.5,
+                depth: 0.2,
+                curveSegments: 25,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 6
+                
+            }
+        )
+
+        const textGeometry2 = new TextGeometry(
+            'Aspiring Creative Developer',
             {
                 font: font,
                 size: 0.5,
@@ -55,17 +71,25 @@ fontLoader.load(
         textGeometry.center()
 
         const text = new THREE.Mesh(textGeometry, material)
+        text.rotation.y=0.25
         scene.add(text)
 
-        // Donuts
-        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+        const text2 = new THREE.Mesh(textGeometry2, material)
+        text2.position.set(-3,-2.5,0)
+        text2.rotation.y=(-0.25)
+        
+        scene.add(text2)
+        
 
-        for(let i = 0; i < 100; i++)
+        // Donuts
+        const donutGeometry = new THREE.RingGeometry(0.6, 0.5, 32, 64)
+
+        for(let i = 0; i < 200; i++)
         {
             const donut = new THREE.Mesh(donutGeometry, material)
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
+            donut.position.x = (Math.random() - 0.5) * 30
+            donut.position.y = (Math.random() - 0.5) * 30
+            donut.position.z = (Math.random() - 0.5) * 30
             donut.rotation.x = Math.random() * Math.PI
             donut.rotation.y = Math.random() * Math.PI
             const scale = Math.random()
@@ -103,10 +127,10 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = -2    
+camera.position.y = -1
+camera.position.z = 6
 scene.add(camera)
 
 // Controls
@@ -117,7 +141,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialiasing: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -130,11 +155,11 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
     // Update controls
     controls.update()
 
     // Render
+    
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
